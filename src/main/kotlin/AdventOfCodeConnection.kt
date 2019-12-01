e0276b5d674107aaa5255ec84c19bd37ea050751
Krzysztof Sroga <krzysztof@sroga.pw>
2019-12-01 17:14:53 +0100

    2018 day 1 solved

--- src/main/kotlin/AdventOfCodeConnection.kt
+++ src/main/kotlin/AdventOfCodeConnection.kt
@@ -4,7 +4,7 @@ import com.github.kittinunf.result.Result
 import java.io.File
 
 object AdventOfCodeConnection {
-    private var year = 2019
+    var year = 2019
 
     init {
         val session = File("session").readText()
@@ -17,6 +17,19 @@ object AdventOfCodeConnection {
         val path = "/$year/day/$day/input"
 
         val (_, _, result) = Fuel.get(path).responseString()
+        when (result) {
+            is Result.Success -> return result.value
+            is Result.Failure -> throw result.error
+        }
+    }
+
+    fun postAnswer(day: Int, level: Int, answer: Int): String {
+        val path = "/$year/day/$day/answer"
+        val parameters = mapOf(
+            "level" to level,
+            "answer" to answer
+        ).toList()
+        val (_, _, result) = Fuel.post(path, parameters = parameters).responseString()
         when (result) {
             is Result.Success -> return result.value
             is Result.Failure -> throw result.error
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,12 +9,38 @@ import java.io.File
 //import kotlin.system.measureTimeMillis
 
 fun main() {
+    AdventOfCodeConnection.year = 2018
     val input = AdventOfCodeConnection.fetchInput(1)
-    val result1 = input.lines().mapNotNull { it.toIntOrNull() }.map(::calculateFuel1).sum()
-    val result2 = input.lines().mapNotNull { it.toIntOrNull() }.map(::calculateFuel2).sum()
-    println("Solution for day 1:")
-    println("part1: $result1")
-    println("part2: $result2")
+    var sum = 0
+    val set = mutableSetOf<Int>()
+
+    aocSolution(1) {
+        mapToIntNotNull().sum()
+    }
+
+    aocSolution(1) {
+        while(true) {
+            mapToIntNotNull().forEach {
+                sum += it
+                if(set.contains(sum)) {
+                    return@aocSolution sum
+                }
+                set+=sum
+            }
+        }
+        0
+    }
+
+//    println(AdventOfCodeConnection.postAnswer(3, 2, 452))
+
+
+
+//
+//    val result1 = input.lines().mapNotNull { it.toIntOrNull() }.map(::calculateFuel1).sum()
+//    val result2 = input.lines().mapNotNull { it.toIntOrNull() }.map(::calculateFuel2).sum()
+//    println("Solution for day 1:")
+//    println("part1: $result1")
+//    println("part2: $result2")
 }
 
 fun calculateFuel1(fuel: Int) = fuel / 3 - 2
@@ -24,4 +50,20 @@ fun calculateFuel2(fuel: Int): Int {
     return if (a > 0) {
         a + calculateFuel2(a)
     } else 0
+}
+
+fun aocSolution(day: Int, solutionCode: List<String>.() -> Int)  {
+    val answer = solutionCode(AdventOfCodeConnection.fetchInput(day).lines())
+    val solution = AocSolution(day, answer)
+    println(solution)
+}
+
+fun Iterable<String>.mapToIntNotNull(): List<Int> = mapNotNull { it.toIntOrNull() }
+
+data class AocSolution(val day: Int, val answer: Int)  {
+    fun postLevel(level: Int) {
+        val responseString = AdventOfCodeConnection.postAnswer(day, level, answer)
+        println("Solution posted")
+        println(responseString)
+    }
 }
